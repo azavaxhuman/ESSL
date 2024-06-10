6c121de154e7fd14d6f30d4dd30090c32bb9bb42
Azavax <21226881+azavaxhuman@users.noreply.github.com>
2024-06-10 19:51:50 +0330

    Update essl.sh

--- essl.sh
+++ essl.sh
@@ -69,7 +69,8 @@ validate_domain() {
 validate_email() {
     while true; do
         read -r -p "Please enter your email: " email
-        if [[ "$email" =~ .*@.*\..* && ${#email} -gt 5 ]]; then
+        if [[ "$email_r" =~ .*@.*\..* && ${#email_r} -gt 5 ]]; then
+        email="$email_r"
             return 0
         else
             error "Invalid email format. Please enter a valid email address."
@@ -80,7 +81,8 @@ validate_email() {
 validate_apikey() {
     while true; do
         read -r -p  "Please enter your Global API key: " api_key
-        if [[ -n "$api_key" ]]; then
+        if [[ -n "$api_key_r" ]]; then
+            api_key="$api_key_r"
             break
         else
             error "API key cannot be empty. Please enter a valid API key."
@@ -420,7 +422,7 @@ print "\t\t forked from @ErfJab\n\n"
 
 while true; do
     print "-------------------------------------------------------"
-    print "V2.0"
+    print "V2.1"
     print "1) New Single Domain ssl (sub.domain.com)"
     print "2) New Wildcard ssl (*.domain.com)"
     print "3) New Multi-Domain ssl (sub.domain1.com, sub2.domain2.com ...)"
@@ -445,7 +447,8 @@ while true; do
             validate_domain
             validate_email 
             validate_apikey
-            clear
+            export CF_Key="$api_key"
+            export CF_Email="$email"
             get_cloudflare_ssl "$domain" "$email" "$api_key"
         else
             error "Invalid option."
@@ -465,8 +468,6 @@ while true; do
             validate_domain
             validate_email 
             validate_apikey
-            export CF_Key="$api_key"
-            export CF_Email="$email"
             get_cloudflare_ssl "$domain" "$email" "$api_key"
         else
             error "Invalid option. Please enter 1 or 2."
